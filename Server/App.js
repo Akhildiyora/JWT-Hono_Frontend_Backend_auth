1af5089138bbe7cb5e3941a1f406110ee3111206
Akhildiyora <akhildiyora2003@gmail.com>
2026-01-01 10:58:53 +0530

    format change

--- Server/App.js
+++ Server/App.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
-import {sign, verify } from 'hono/jwt';
+import { sign, verify } from 'hono/jwt';
 import bcrypt from 'bcrypt';
 import { cors } from 'hono/cors';
 import db from './Database/database.js';
@@ -11,15 +11,15 @@ const JWT_SECRET = process.env.SECRET
 const app = new Hono();
 app.use('*', cors({
     origin: 'http://localhost:5173',
-    }))
+}))
 
 app.get('/', (c) => c.text('working'))
 
-app.post('/create', async(c) => {
+app.post('/create', async (c) => {
     const { username, email, password, age } = await c.req.json();
     const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email);;
-    if(user){
-        return c.json({message: "already Registered"}, 400);
+    if (user) {
+        return c.json({ message: "already Registered" }, 400);
     }
     const hash = await bcrypt.hash(password, 10);
     db.prepare(`
@@ -27,43 +27,43 @@ app.post('/create', async(c) => {
     VALUES (?, ?, ?, ?)
   `).run(username, email, hash, age);
 
-    return c.json({success: true, message: "User Created Successfully",},201)
+    return c.json({ success: true, message: "User Created Successfully", }, 201)
 })
 
 app.post('/login', async (c) => {
     const { email, password } = await c.req.json();
-const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email);
-        if(!user || !user.password){
-        return c.json({error: 'Somthing is Missing'},400)
+    const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email);
+    if (!user || !user.password) {
+        return c.json({ error: 'Somthing is Missing' }, 400)
     }
     const result = await bcrypt.compare(password, user.password)
-    if(!result){
-        return c.json({error: "you can't login"}, 401)
+    if (!result) {
+        return c.json({ error: "you can't login" }, 401)
     }
-    const token = await sign({email:user.email, exp: Math.floor(Date.now()/1000) + 60 * 60}, JWT_SECRET)
-    
-    return c.json({success:true, message:'Login Successful', token, user:{email: user.email, username: user.username,},})
+    const token = await sign({ email: user.email, exp: Math.floor(Date.now() / 1000) + 60 * 60 }, JWT_SECRET)
+
+    return c.json({ success: true, message: 'Login Successful', token, user: { email: user.email, username: user.username, }, })
 })
 
 const authMiddleware = async (c, next) => {
     const authHeader = c.req.header('Authorization')
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        return c.json({error: 'Unauthorized'}, 401)
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return c.json({ error: 'Unauthorized' }, 401)
     }
     const token = authHeader.split(' ')[1]
-    try{
+    try {
         const payload = await verify(token, JWT_SECRET)
         c.set('user', payload)
         await next()
     } catch {
-        return c.json({ error: 'Invalid or expired token'}, 401)
+        return c.json({ error: 'Invalid or expired token' }, 401)
     }
 }
 
 app.get('/profile', authMiddleware, (c) => {
     const payload = c.get('user');
-const user = db.prepare(`SELECT id, username, email, age FROM users WHERE email = ?`).get(payload.email);
-    return c.json({ message: 'protected route', user});
+    const user = db.prepare(`SELECT id, username, email, age FROM users WHERE email = ?`).get(payload.email);
+    return c.json({ message: 'protected route', user });
 })
 
 app.get('/logout', (c) => {
